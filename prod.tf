ac4701de9e7d90cdab18d91d93ab2bc0439050f3
Aaron Ortega <aaortega18@gmail.com>
2020-05-16 16:24:10 -0500

    defined initial web security group

--- prod.tf
+++ prod.tf
@@ -5,10 +5,39 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
-  # tf_course is the resource name according to terraform
-  # resource name according to aws
+  # prod_tf_course is the resource name according to terraform
+  # while bucket is the resource according to aws
   bucket = "tf-course-ortega-aaron"
   acl = "private"
 }
 
 resource "aws_default_vpc" "default" {}  # use default features
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"  # aws name
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]  # everything
+  }
+  ingress {
+    from_port = 443
+    protocol  = "tcp"
+    to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    # Tag to identify which resource is managed by terraform
+    "Terraform" : "true"
+  }
+}
