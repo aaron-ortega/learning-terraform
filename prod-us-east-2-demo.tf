d278537f694dbc8316a0fd70e81fb0705cf3b853
Aaron Ortega <aortega@farmersfridge.com>
2021-02-13 15:56:15 -0600

    defined initial web security group

--- prod-us-east-2-demo.tf
+++ prod-us-east-2-demo.tf
@@ -4,7 +4,7 @@ Notes:
 */
 
 provider "aws" {
-//  Provider gives access to resource(s)
+  //  Provider gives access to resource(s)
   profile = "terraform-user"
   region  = "us-east-2"
 }
@@ -16,9 +16,37 @@ How to make a resource?
   3. Pass arguments to resource
 */
 resource "aws_s3_bucket" "prod_tf_course" {
-  bucket = "tf-course-20200213-ortega"  # globally unique
+  bucket = "tf-course-20200213-ortega" # globally unique
   acl    = "private"
 }
 
+// No arguments passed; use default values
 resource "aws_default_vpc" "default" {}
 
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web" # name that AWS will use if empty a random name will be assigned
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"] # list of IP address that this rule will apply to
+  }
+  ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"  # allow all
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
