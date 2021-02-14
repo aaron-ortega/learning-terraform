d1ab3f28254b5cb52efd28d397323c19ad1514fd
Aaron Ortega <aortega@farmersfridge.com>
2021-02-14 10:45:23 -0600

    load balancer for prod_web

--- prod-us-east-2-demo.tf
+++ prod-us-east-2-demo.tf
@@ -23,6 +23,22 @@ resource "aws_s3_bucket" "prod_tf_course" {
 // No arguments passed; use default values
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-2a"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-2b"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
 resource "aws_security_group" "prod_web" {
   name        = "prod_web" # name that AWS will use if empty a random name will be assigned
   description = "Allow standard http and https ports inbound and everything outbound"
@@ -75,6 +91,24 @@ resource "aws_eip_association" "prod_web" {
 
 // Add static IP (we can keep the IP address even if we destroy the image)
 resource "aws_eip" "prod_web" {
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
   tags = {
     "Terraform" : "true"
   }
